09eff1f5ef94230fc0c4f1b4aa1e85fabd106aa2
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-24 21:51:02 +0530

    Create App.jsx

---
+++ projectfile(Resistation page)/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UseState2 from './componet/UseState2'
+
+function App(){
+  return (
+    <React.Fragment>
+    <UseState2 />
+    </React.Fragment>
+  )
+}
+
+
+export default App
