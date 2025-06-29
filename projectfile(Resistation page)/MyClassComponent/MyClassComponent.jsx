f532e739f5f211a959033c674304c01b91c579ae
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 05:23:26 +0530

    Delete projectfile(Resistation page)/MyClassComponent/MyClassComponent.jsx

--- projectfile(Resistation page)/MyClassComponent/MyClassComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-class MyClassComponent extends React.Component{
-    render(){
-        return(
-            <h3>This is MyClassComponent</h3>
-        )
-    }
-}
-export default MyClassComponent
