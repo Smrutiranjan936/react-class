97b6e567fb13fb8fdc9ce456033e06c4e5582453
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 05:22:13 +0530

    Create App.jsx

---
+++ projectfile(Resistation page)/MyClassComponent/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import MyClassComponent from "./component/MyClassComponent"
+function App() {
+  return (
+
+    <React.Fragment>
+      <h1>
+        Hello i am Smrutiranjan From puri.
+      </h1>
+      <MyClassComponent />
+    </React.Fragment>
+
+  )
+}
+export default App
