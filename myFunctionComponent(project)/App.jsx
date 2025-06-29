ec778e9e2ec415c5afc9a3a061ba653b51720ff6
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-29 11:49:47 +0530

    Create App.jsx

---
+++ myFunctionComponent(project)/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import MyFuntanalComponent from "./component/MyFuntanalComponent"
+import MyClassComponent from "./component/MyClassComponent"
+function App() {
+  return (
+
+    <React.Fragment>
+      <h1>
+        Hello i am Smrutiranjan From puri.
+      </h1>
+      <MyFuntanalComponent />
+      <MyClassComponent />
+    </React.Fragment>
+
+  )
+}
+
+
+export default App
