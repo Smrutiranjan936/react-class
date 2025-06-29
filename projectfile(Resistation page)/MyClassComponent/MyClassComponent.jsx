3ebdc14cd0e887f752e59be4304a8c863951908e
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 05:22:55 +0530

    Create MyClassComponent.jsx

---
+++ projectfile(Resistation page)/MyClassComponent/MyClassComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+class MyClassComponent extends React.Component{
+    render(){
+        return(
+            <h3>This is MyClassComponent</h3>
+        )
+    }
+}
+export default MyClassComponent
