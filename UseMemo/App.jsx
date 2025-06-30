c867c389bef44a9f2762293d5822fc98daef8993
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 05:30:41 +0530

    Create App.jsx

---
+++ UseMemo/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UseMemo from './componet/UseMemo'
+
+function App(){
+  return (
+    <React.Fragment>
+    <UseMemo />
+    </React.Fragment>
+  )
+}
+export default App
