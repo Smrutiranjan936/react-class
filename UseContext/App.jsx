65ddd7551b08d54e3cf799f36adbae71d3c7ea26
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 06:00:09 +0530

    Create App.jsx

---
+++ UseContext/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UseContext from './componet/UseContext'
+function App(){
+  return (
+    <React.Fragment>
+    <UseContext />
+    </React.Fragment>
+  )
+}
+export default App
