ee56cce31593234843af38a103b630b47b2842f1
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 13:29:04 +0530

    Create App.jsx

---
+++ DataMapping/App.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+import DataMapping from './component/DataMapping'
+
+function App() {
+  return (
+    <React.Fragment>
+    < DataMapping/> 
+    </React.Fragment>
+  )
+}
+
+export default App
