f6b7cb4ad0c37e221c03f9be450c67d8a73d197c
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 05:38:23 +0530

    Create App.jsx

---
+++ ChangeBG/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ChangeBG from './componet/ChangeBG'
+function App(){
+  return (
+    <React.Fragment>
+    <ChangeBG />
+    </React.Fragment>
+  )
+}
+export default App
