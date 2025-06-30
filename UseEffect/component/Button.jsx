633082a4d396ddf4649ea85fcc1e689e206a036a
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 14:41:31 +0530

    Delete UseEffect/component/Button.jsx

--- UseEffect/component/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react";
-
-function Button({ handleClick, text }) {
-    console.log('button')
-    return (
-        <button onClick={handleClick}>{text}</button>
-    )
-}
-export default Button;
