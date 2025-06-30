381236581a8945feb3da6d6fbb1a88f774e30809
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-06-30 05:48:19 +0530

    Create App.jsx

---
+++ UseCallBack/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UseCallBack from './componet/UseCallback'
+function App(){
+  return (
+    <React.Fragment>
+    <UseCallBack />
+    </React.Fragment>
+  )
+}
+export default App
