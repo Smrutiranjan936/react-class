7842281398ff2b3587c64d9671c29c6c32be05a7
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-07-07 12:55:42 +0530

    Update App.jsx

--- Button/App.jsx
+++ Button/App.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react'
+import Button from './componet/Button'
+
+function App(){
+  return (
+    <React.Fragment>
+    <Button/>
+    </React.Fragment>
+  )
+}
+export default App
