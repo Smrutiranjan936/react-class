44130e29060671a5f24498bc2de0f9d27c45ecf1
Smrutiranjan Mallick <149951746+Smrutiranjan936@users.noreply.github.com>
2025-07-07 13:04:04 +0530

    Update App.jsx

--- UseEffect/App.jsx
+++ UseEffect/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import UseEffect from './componet/UseEffect'
 
+function App(){
+  return (
+    <React.Fragment>
+    <UseEffect/>
+    </React.Fragment>
+  )
+}
+
+export default App
